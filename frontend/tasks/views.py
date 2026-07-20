b2f95e5afea18ed1a0e4982aee23fae84b0c811d
Janujagruti <ranejanhavi1@gmail.com>
2026-07-20 12:56:02 +0530

    In dashboard task display and task add completed

--- frontend/tasks/views.py
+++ frontend/tasks/views.py
@@ -1,5 +1,5 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def dashboard(request):
     response = requests.get("http://127.0.0.1:8000/api/")
@@ -19,7 +19,27 @@ def logout_view(request):
     pass
 
 def add_task(request):
-    pass
+
+    if request.method == "POST":
+
+        data = {
+            "title": request.POST.get("title"),
+            "description": request.POST.get("description"),
+            "status": request.POST.get("status"),
+            "priority": request.POST.get("priority"),
+            "due_date": request.POST.get("due_date"),
+        }
+
+        response = requests.post(
+            "http://127.0.0.1:8000/api/",
+            data=data
+        )
+
+        if response.status_code == 201:
+            return redirect("dashboard")
+
+
+    return render(request, "add_task.html")
 
 def edit_task(request, task_id):
     pass
